9a467150c52866aa050d38ac7f1d8af0a1415903
Surhoka <danialsurhoka@gmail.com>
2025-11-05 22:09:08 +0700

    Delete admin-sidebar-loader.js

--- admin-sidebar-loader.js
+++
@@ -1,9 +0,0 @@
-// Define a global function to load the admin sidebar
-window.loadAdminSidebar = () => {
-    // Ensure buildNav and adminNavLinksData are available globally
-    if (window.buildNav && window.adminNavLinksData) {
-        window.buildNav(window.adminNavLinksData);
-    } else {
-        console.error('Required functions or data for admin sidebar not found on window object.');
-    }
-};
