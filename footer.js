74a0360fbb3eaf9127274336a75fc5568605b2e0
Surhoka <danialsurhoka@gmail.com>
2025-11-05 22:14:33 +0700

    Delete footer.js

--- footer.js
+++
@@ -1,7 +0,0 @@
-export function getFooterHtml() {
-  return `
-    <div class="text-center my-3">
-      Designed by <a href="https://tailadmin.com/" target="_blank" class="text-blue-600">TailAdmin</a>
-    </div>
-  `;
-}
