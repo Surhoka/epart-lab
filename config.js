4c465913016c92e8bb6ab1c735c3df7b5dec8f41
Surhoka <danialsurhoka@gmail.com>
2025-11-05 22:10:31 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// File: src/config.js
-// Deskripsi: Menyimpan konfigurasi utama aplikasi.
-
-// Ganti dengan URL blog Anda
-export const BLOG_URL = 'https://www.blogger.com'; 
-export const POSTS_PER_PAGE = 10; // Jumlah post per halaman
