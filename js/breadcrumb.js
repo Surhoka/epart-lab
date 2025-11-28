901fff49d83dac89bc1550ef64af3a34cb8e8b6b
Surhoka <danialsurhoka@gmail.com>
2025-11-28 16:10:42 +0700

    Update breadcrumb.js

--- js/breadcrumb.js
+++ js/breadcrumb.js
@@ -3,7 +3,7 @@ window.renderBreadcrumb = function (pageName, containerId = 'breadcrumb-containe
     if (!container) return;
 
     container.innerHTML = `
-      <div class="mb-6 flex flex-wrap items-center justify-between gap-3">
+      <div class="mb-6 flex flex-col gap-3">
         <h2 class="text-xl font-semibold text-gray-800 dark:text-white/90">${pageName}</h2>
 
         <nav>
