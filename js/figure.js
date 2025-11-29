f35f6f664913457f926e540e6c94df84fe5042de
Surhoka <danialsurhoka@gmail.com>
2025-11-30 00:57:27 +0700

    Update figure.js

--- js/figure.js
+++ js/figure.js
@@ -58,9 +58,6 @@ window.initFigurePage = function () {
 
         gridContainer.innerHTML = data.map(item => `
           <div class="rounded-2xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03]">
-            <div class="mb-4 flex items-center justify-between">
-              <span class="text-sm font-medium text-gray-500 dark:text-gray-400">${item.Figure}</span>
-            </div>
             <div class="mb-4 flex justify-center">
                 <div class="flex w-full items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-800">
                     ${item.FigureUrl ? `<img src="${item.FigureUrl}" alt="${item.Title}" class="w-full h-auto object-contain rounded-lg" />` :
@@ -69,7 +66,7 @@ window.initFigurePage = function () {
             </div>
             <div>
               <h4 class="text-lg font-semibold text-gray-800 dark:text-white/90">
-                ${item.Title}
+                ${item.Figure} | ${item.Title}
               </h4>
               <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">${item.VehicleModel}</p>
             </div>
