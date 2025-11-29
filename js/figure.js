2275c65cf9c46b585cc6ab7e76a47b6b6f5b2794
Surhoka <danialsurhoka@gmail.com>
2025-11-30 01:02:25 +0700

    Update figure.js

--- js/figure.js
+++ js/figure.js
@@ -59,15 +59,15 @@ window.initFigurePage = function () {
         gridContainer.innerHTML = data.map(item => `
           <div class="rounded-2xl border border-gray-200 bg-white p-4 dark:border-gray-800 dark:bg-white/[0.03]">
             <div class="mb-4 flex justify-center">
-                <div class="flex w-full items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-800">
-                    ${item.FigureUrl ? `<img src="${item.FigureUrl}" alt="${item.Title}" class="w-full h-auto object-contain rounded-lg" />` :
-                `<div class="flex h-48 w-full items-center justify-center"><svg class="h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg></div>`}
+                <div class="flex aspect-[4/3] w-full items-center justify-center rounded-lg bg-gray-50 dark:bg-gray-800 overflow-hidden">
+                    ${item.FigureUrl ? `<img src="${item.FigureUrl}" alt="${item.Title}" class="h-full w-full object-contain" />` :
+                `<div class="flex h-full w-full items-center justify-center"><svg class="h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg></div>`}
                 </div>
             </div>
             <div>
               <h4 class="text-lg font-semibold text-gray-800 dark:text-white/90">
                 ${item.Figure} | ${item.Title}
-              </h4>
+              </h3>
               <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">${item.VehicleModel}</p>
             </div>
           </div>
