984f6bbed672d264bbe4b3b341ee6d2429d4d16e
Surhoka <danialsurhoka@gmail.com>
2025-11-30 01:54:18 +0700

    Update figure.js

--- js/figure.js
+++ js/figure.js
@@ -64,9 +64,8 @@ window.initFigurePage = function () {
                 `<div class="flex h-48 w-full items-center justify-center"><svg class="h-12 w-12 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg></div>`}
                 </div>
             </div>
-            <div>
             <div class="flex flex-col justify-between min-h-[4.5rem]">
-              <h3 class="text-lg font-semibold text-gray-500 dark:text-white/90">
+              <h3 class="text-lg font-semibold text-gray-500 dark:text-white/90line-clamp-1">
                 ${item.Figure} | ${item.Title}
               </h3>
               <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">${item.VehicleModel}</p>
@@ -119,3 +118,4 @@ window.initFigurePage = function () {
         });
     }
 };
+
