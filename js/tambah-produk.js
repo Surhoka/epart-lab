823459379acd0d0afca61e43242938fa2bf4fbb1
Surhoka <danialsurhoka@gmail.com>
2025-12-13 22:02:13 +0700

    Update tambah-produk.js

--- js/tambah-produk.js
+++ js/tambah-produk.js
@@ -230,7 +230,7 @@ window.initTambahProdukPage = function () {
                 else newSaveButton.disabled = false;
             };
 
-            window.sendDataToGoogle('simpanProdukBaru', productData, successCallback, errorCallback);
+            window.sendDataToGoogle('simpanProduk', productData, successCallback, errorCallback);
         });
     } else {
         console.error("Tombol Simpan (saveProductBtn) tidak ditemukan saat inisialisasi.");
