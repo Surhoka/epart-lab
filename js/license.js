2f12ee957ba3bbe170f9fb12e6375cc0adfc5ec6
Surhoka <danialsurhoka@gmail.com>
2026-03-12 00:29:44 +0700

    Add files via upload

--- js/license.js
+++ js/license.js
@@ -1,13 +1,21 @@
 /**
  * License Page Component
  */
-document.addEventListener('alpine:init', () => {
-    Alpine.data('licensePage', () => ({
-        isLoading: false,
-        lastUpdated: '2026-03-12',
+const registerLicensePage = () => {
+    if (window.Alpine && !window.Alpine.data('licensePage')) {
+        window.Alpine.data('licensePage', () => ({
+            isLoading: false,
+            lastUpdated: '2026-03-12',
 
-        init() {
-            console.log('License Page Initialized');
-        }
-    }));
-});
+            init() {
+                console.log('License Page Initialized');
+            }
+        }));
+    }
+};
+
+if (window.Alpine) {
+    registerLicensePage();
+} else {
+    document.addEventListener('alpine:init', registerLicensePage);
+}
