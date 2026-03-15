5e847d24505748fe007afeb277405f8640bb98ba
Surhoka <danialsurhoka@gmail.com>
2026-03-15 23:21:06 +0700

    Add files via upload

---
+++ public-js/contact.js
@@ -0,0 +1,10 @@
+/**
+ * Contact Page Logic for Public EzyParts
+ */
+window.initContactPage = function () {
+    return {
+        init() {
+            console.log('Contact page initialized');
+        }
+    };
+};
