2df1be320d5a5749d1b0df23cb7d7d8093a503b7
Surhoka <danialsurhoka@gmail.com>
2026-03-15 23:10:57 +0700

    Add files via upload

---
+++ public-js/about.js
@@ -0,0 +1,11 @@
+/**
+ * About Page Logic for Public EzyParts
+ */
+window.initAboutPage = function () {
+    return {
+        init() {
+            // About page initialization logic if needed
+            console.log('About page initialized');
+        }
+    };
+};
