d866eb3029e825d71a162b0d8e16ab0e61fc03e9
Danial Surhoka <danialsurhoka@gmail.com>
2026-04-28 23:06:18 +0700

    Add files via upload

--- js/database.js
+++ js/database.js
@@ -23,21 +23,17 @@ window.database = function () {
 
             window.sendDataToGoogle('get_db_hierarchy', {}, (response) => {
                 this.isLoading = false;
-                console.log("DEBUG: Server Response:", response);
                 if (response.data) {
                     this.hierarchy = response.data;
-                    console.log("DEBUG: Hierarchy Set:", JSON.stringify(this.hierarchy));
                     // Auto expand root
                     if (this.hierarchy.length > 0) {
                         this.expandedNodes[this.hierarchy[0].id] = true;
                     }
                 } else {
-                    console.warn("DEBUG: No data in response.");
                     this.hierarchy = [];
                 }
             }, (err) => {
                 this.isLoading = false;
-                console.error("DEBUG: Fetch Error:", err);
                 this.error = err.message || "Failed to fetch database structure.";
             });
         },
@@ -47,7 +43,6 @@ window.database = function () {
         },
 
         openInDrive(url) {
-            console.log("DEBUG: openInDrive called with:", url);
             if (url) {
                 window.open(url, '_blank');
             } else {
@@ -57,16 +52,11 @@ window.database = function () {
         },
 
         openRootInDrive() {
-            console.log("DEBUG: openRootInDrive triggered. Current state:", this.hierarchy);
             const root = (this.hierarchy && this.hierarchy.length > 0) ? this.hierarchy[0] : null;
             if (root) {
                 const targetUrl = root.url || ("https://drive.google.com/drive/folders/" + root.id);
-                console.log("DEBUG: Target URL determined:", targetUrl);
-                alert("DEBUG Navigasi:\nID: " + root.id + "\nURL: " + targetUrl);
                 window.open(targetUrl, '_blank');
             } else {
-                console.error("DEBUG: Hierarchy is empty or root missing.");
-                alert("DEBUG: Hierarchy masih kosong atau root tidak ditemukan.");
                 window.open('https://drive.google.com/drive/my-drive', '_blank');
             }
         }
