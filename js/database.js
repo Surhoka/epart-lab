c8f18bd93a66207e4205f59cfe1c8488717e91be
Danial Surhoka <danialsurhoka@gmail.com>
2026-04-28 17:10:30 +0700

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,54 @@
+
+/**
+ * database.js
+ */
+
+window.database = function () {
+    return {
+        // State
+        isLoading: true,
+        error: null,
+        hierarchy: [], // List of { name, type, url, children: [] }
+        expandedNodes: {}, // ID atau tipe yang otomatis terbuka (menggunakan objek untuk reaktivitas)
+
+        // Init
+        init() {
+            this.isLoading = true;
+            this.refreshStructure();
+        },
+
+        refreshStructure() {
+            this.isLoading = true;
+            this.error = null;
+
+            window.sendDataToGoogle('get_db_hierarchy', {}, (response) => {
+                this.isLoading = false;
+                if (response.data) {
+                    this.hierarchy = response.data;
+                    // Auto expand root
+                    if (this.hierarchy.length > 0) {
+                        this.expandedNodes[this.hierarchy[0].id] = true;
+                    }
+                } else {
+                    this.hierarchy = [];
+                }
+            }, (err) => {
+                this.isLoading = false;
+                this.error = err.message || "Failed to fetch database structure.";
+            });
+        },
+
+        toggleNode(id) {
+            this.expandedNodes[id] = !this.expandedNodes[id];
+        },
+
+        openInDrive(url) {
+            if (url) {
+                window.open(url, '_blank');
+            } else {
+                if (window.showAlert) window.showAlert('error', 'URL tidak tersedia untuk item ini.');
+                else alert('URL tidak tersedia untuk item ini.');
+            }
+        }
+    }
+}
