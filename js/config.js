55761c7f2e3e14c45c3bf2c82575169eab252fbf
Danial Surhoka <danialsurhoka@gmail.com>
2026-05-01 00:33:56 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,7 +1,15 @@
 const CONFIG = {
-    WEBAPP_URL_DEV: 'https://script.google.com/macros/s/AKfycbzMzHmga1ChGhHstTFSrUkFdJoMO5Y0lYxlNBFXwRWvXjJ1PeSUty4GD6lBLa5rA7jV/exec',//setup.gs URL
-    // Add other environments like PROD if needed
+    WEBAPP_URL_DEV: 'aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J6TXpIbWdhMUNoR2hIc3RURlNyVWtGZEpvTU81WTBsWXhsTkJGWHdSV3ZYakoxUGVTVXR5NEdENmxCTGE1ckE3alYvZXhlYw==',
 };
+
+function decodeBase64(base64String) {
+    try {
+        return atob(base64String);
+    } catch (e) {
+        console.error('Gagal mendekode Base64:', e);
+        return '';
+    }
+}
 
 function getConfig() {
     try {
@@ -15,15 +23,19 @@ function getConfig() {
 
 function getWebAppUrl() {
     const config = getConfig();
+
     if (config && config.webappUrl && config.webappUrl.startsWith('http')) {
+
         return config.webappUrl;
     }
-    return '';
+
+    return decodeBase64(CONFIG.WEBAPP_URL_DEV);
 }
 
 function getDatabaseConfig() {
     const config = getConfig();
     return config ? {
+
         dbName: config.dbName || '',
         sheetId: config.sheetId || '',
         dbSetup: config.dbSetup || 'auto'
